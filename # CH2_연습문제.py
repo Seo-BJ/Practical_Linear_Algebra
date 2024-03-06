d2cb521d84fb201f892ae33a299a5cbdfef9f733
서범준 <binijuni00@gmail.com>
2024-03-06 22:29:18 +0900

    Create # CH2_연습문제.py

---
+++ # CH2_연습문제.py
@@ -0,0 +1,20 @@
+# CH2_연습문제
+# 정답 링크 : https://github.com/Sancho-kim/LinAlg4DS
+
+# import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+# NOTE: these lines define global figure properties used for publication.
+import matplotlib_inline.backend_inline
+matplotlib_inline.backend_inline.set_matplotlib_formats('svg') # display figures in vector format
+plt.rcParams.update({'font.size':14}) # set global font size
+
+u = np.array([0,0,1])
+v = np.array([1,2,3])
+
+# 2-1
+
+# 2-2
+
+# 2-3
